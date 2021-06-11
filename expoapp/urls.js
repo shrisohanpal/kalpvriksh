e42b20a162583fb0a90a265195651142decdcf84
Darshan <shrisohanpal@gmail.com>
2021-06-11 14:24:04 +0530

    some bug fixed

--- expoapp/urls.js
+++ expoapp/urls.js
@@ -1,10 +1,5 @@
 //const baseUrl = 'http://192.168.43.102:5000'
 
-const baseUrl = 'http://kalpvriksh.co.in'
-// Add permission in android manifest for http
-/**
-<application android:usesCleartextTraffic="true">
-</application>
- */
+const baseUrl = 'https://kalpvriksh.co.in' // Always use https ! http will not work in android version 9,10
 
 export { baseUrl }
